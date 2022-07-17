3949b05c517b9950f0e504082cb3c7e4e1f7da7e
José Pullaguari <jepullaguari@utpl.edu.ec>
2022-07-17 10:31:47 -0500

    test-deployment

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(8080, '0.0.0.0');
+  console.log('Running on http://0.0.0.0:8080');
+}
+bootstrap();
