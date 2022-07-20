15ba5c934dff10be4844a948fc473cb222ed1992
José Pullaguari <jepullaguari@utpl.edu.ec>
2022-07-20 18:27:50 -0500

    Actualizacion de google.yml

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,6 +2,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World Welcome to Micro-Reservas';
+    return 'Hello World Micro-Reservas';
   }
 }
